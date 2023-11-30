74c34687c729391b6f17b54272aa1686abf275eb
ArtyomKuzmin <aramas.artt@yandex.ru>
2023-11-30 19:58:07 +0400

    fix2

--- lab_5/task-5-18.sql
+++ lab_5/task-5-18.sql
@@ -6,19 +6,23 @@ WITH RECURSIVE DateRange AS (
     UNION ALL
     SELECT DATE_ADD(Date, INTERVAL 1 DAY) 
     FROM DateRange 
-    WHERE Date < '2012-08-31'),
+    WHERE Date < '2012-08-31'
+),
 TotalRevenue AS(
 SELECT DateRange.Date, SUM(
 		CASE 
 			WHEN(DATE(b.starttime) != Date) THEN 0
             WHEN (b.memid = 0) THEN f.guestcost * b.slots
             ELSE f.membercost * b.slots
-        END) AS доход
-		(SUM(IF(b.memid = 0, f.guestcost, f.membercost) * b.slots) / 15) as среднее
+        END) AS Доход,
+		(SUM(
+        IF(b.memid = 0, f.guestcost, f.membercost) * b.slots
+    ) / 15) as Среднее_скользящее
     FROM DateRange
     LEFT JOIN bookings b ON DATE_ADD(Date, INTERVAL -14 day) <= DATE(b.starttime) AND DATE(b.starttime) <= Date
     LEFT JOIN facilities f ON b.facid = f.facid
     LEFT JOIN members m ON b.memid = m.memid
-    GROUP BY Date)
+    GROUP BY Date
+)
 SELECT * FROM TotalRevenue
 ORDER BY Date;	
