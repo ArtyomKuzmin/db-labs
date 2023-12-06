b4909457a4f6d3bb493c12347e60d8d1e95aaf0f
ArtyomKuzmin <aramas.artt@yandex.ru>
2023-12-06 19:15:27 +0400

    fix

--- lab_7/task-7-01.sql
+++ lab_7/task-7-01.sql
@@ -1,15 +1,19 @@
 -- Создайте функцию, которая рассчитывает стоимость каждой аренды (для каждой записи таблицы bookings).  
 USE cd;
-DELIMITER |
-DROP FUNCTION IF EXISTS cost_of |
-CREATE FUNCTION cost_of(memid INT, facid INT, slots INT) RETURNS INT
-  READS SQL DATA
-  NOT DETERMINISTIC
-  BEGIN
+DELIMITER ||
+DROP FUNCTION IF EXISTS getRentCost ||
+CREATE FUNCTION getRentCost(memid INT, facid INT, slots INT) RETURNS DECIMAL(10, 2)
+    READS SQL DATA
+    NOT DETERMINISTIC
+BEGIN
     DECLARE income INT;
-    SELECT IF(memid = 0, guestcost, membercost) *  slots 
-    INTO income FROM facilities WHERE facid = facilities.facid;
+    SET income = (
+        SELECT IF(memid = 0, guestcost, membercost) * slots
+        FROM facilities
+        WHERE facid = facilities.facid
+    );
     RETURN income;
-  END |
+END ||
 DELIMITER ;
-SELECT cost_of(memid, facid, slots) FROM bookings;
+SELECT getRentCost(memid, facid, slots)
+FROM bookings;
