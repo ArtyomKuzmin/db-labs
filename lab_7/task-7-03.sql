bc99fbbe17cc33950f9959dd3f0177ecb26359a2
ArtyomKuzmin <aramas.artt@yandex.ru>
2023-12-25 02:38:02 +0400

    fix

--- lab_7/task-7-03.sql
+++ lab_7/task-7-03.sql
@@ -1,22 +1,22 @@
 -- напишите процедуру, которая считает окупаемость каждого объекта клуба на основании оплаты аренд за месяц. 
 -- применить ее к июлю 2012 года
 USE cd;
+
 DELIMITER $$
-DROP PROCEDURE IF EXISTS getPayback $$
-CREATE PROCEDURE getPayback(whichDate DATE)
-    READS SQL DATA
-    NOT DETERMINISTIC
-BEGIN
-    SELECT
-        b.facid
-    ,   f.facility
-    ,   SUM(p.payment) - f.monthlymaintenance AS income
-    FROM bookings AS b
-    JOIN payments AS p ON b.bookid = p.bookid
-    JOIN facilities AS f ON b.facid = f.facid
-    WHERE DATE_FORMAT(starttime, '%y %m') = DATE_FORMAT(whichDate, '%y %m')
-    GROUP BY b.facid
-    ORDER BY b.facid;
-END $$
+
+DROP PROCEDURE IF EXISTS payback $$
+CREATE PROCEDURE payback(currentDate DATE)
+READS SQL DATA
+NOT DETERMINISTIC
+  BEGIN
+    SELECT book.facid, fac.facility, SUM(pay.payment) - fac.monthlymaintenance AS Доход
+	FROM bookings AS book
+	JOIN payments AS pay ON book.bookid = pay.bookid
+	JOIN facilities AS fac ON book.facid = fac.facid
+	WHERE DATE_FORMAT(starttime, '%y %m') = DATE_FORMAT(currentDate, '%y %m')
+	GROUP BY book.facid ORDER BY book.facid;
+  END $$
+
 DELIMITER ;
-CALL getPayback('2012-07-03');
+
+CALL payback('2012-07-18');
